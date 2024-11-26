8fa7f1c2c7ff50c161d92b010295546c8feda8a3
Alex Gascho <gaschoa@mcmaster.ca>
2024-11-26 18:05:41 -0500

    started gameMechs

--- GameMechs.cpp
+++ GameMechs.cpp
@@ -2,12 +2,28 @@
 
 GameMechs::GameMechs()
 {
-    
+    exitFlag = false;
+    loseFlag = false;
+    input = 0;
+    score = 0;
+
+    boardSizeX = 30;
+    boardSizeY = 15;
+
+    //Add food
 }
 
 GameMechs::GameMechs(int boardX, int boardY)
 {
-    
+    exitFlag = false;
+    loseFlag = false;
+    input = 0;
+    score = 0;
+
+    boardSizeX = boardX;
+    boardSizeY = boardY;
+
+    //Add food
 }
 
 // do you need a destructor?
@@ -18,59 +34,59 @@ GameMechs::~GameMechs()
 
 bool GameMechs::getExitFlagStatus() const
 {
-
+    return exitFlag;
 }
 
 bool GameMechs::getLoseFlagStatus() const
-{
-
+{ 
+    return loseFlag;
 }
     
 
 char GameMechs::getInput() const
 {
-
+    return input;
 }
 
 int GameMechs::getScore() const
 {
-
+    return score;
 }
 
 void GameMechs::incrementScore()
 {
-    
+    score++;
 }
 
 int GameMechs::getBoardSizeX() const
 {
-
+    return boardSizeX;
 }
 
 int GameMechs::getBoardSizeY() const
 {
-
+    return boardSizeY;
 }
 
 
 void GameMechs::setExitTrue()
 {
-
+    exitFlag = true;
 }
 
 void GameMechs::setLoseFlag()
 {
-    
+    loseFlag = true;
 }
 
 void GameMechs::setInput(char this_input)
 {
-
+    input = this_input;
 }
 
 void GameMechs::clearInput()
 {
-
+    input = 0;
 }
 
 // More methods should be added here
